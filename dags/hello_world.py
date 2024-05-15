ac6f1e450e236276af74dfe4291503b41f412fb3
Anil Kumar K B <110081934+kbanil1105@users.noreply.github.com>
2024-05-15 15:36:28 +0530

    Update hello_world.py

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -1,19 +1,18 @@
 from datetime import datetime
+
 from airflow import DAG
-from airflow.operator.bash import BashOperator
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
 
-with DAG(
-dag_id="hello_world",
-start_date=datetime(2024, 4, 24)
-schedule="@once",
-tags=['tutorial']
-)as dag:
-  # Tasks are represented as operators
-  task1 = BashOperator(task_id="task1", bash_commad="echo task1")
-  task1 = BashOperator(task_id="task2", bash_commad="echo task2")
-  task1 = BashOperator(task_id="task3", bash_commad="echo task3")
-  task1 = BashOperator(task_id="task4", bash_commad="echo task4")
+# A DAG represents a workflow, a collection of tasks
+with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
 
+    # Tasks are represented as operators
+    hello = BashOperator(task_id="hello", bash_command="echo hello")
 
- # set dependencies between tasks
- task1 >> task2 >> task3 >> task4
+    @task()
+    def airflow():
+        print("airflow")
+
+    # Set dependencies between tasks
+    hello >> airflow()
